d155fbbe2434d552f8b02694016ef93720bf728a
Carlos Antunes <carlosantunes@pop-os.localdomain>
2022-10-16 22:49:56 -0300

    UserEntity

--- src/infrastructure/repositories/user_repository.py
+++ src/infrastructure/repositories/user_repository.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 from src.infrastrucuture.config import DBConnectionHandler
-from src.infrastrucuture.entities.User import User
+from src.domain.models import User
+from src.infrastrucuture.entities.User import User as UserEntity
 
 
 class UserRepository:
@@ -8,20 +9,19 @@ class UserRepository:
     User repository
     """
 
-    def insert(self, name:str, password:str) -> User:
+    def insert(self, name:str, password:str) -> UserEntity:
         """
         Insert a new user
         params: name, password
         return: None
         """
-        inserted_data = namedtuple('User', ['id', 'name', 'password'])
         with DBConnectionHandler() as db:
             try:
                 user = User(name=name, password=password)
                 db.session.add(user)
                 db.session.commit()
 
-                return inserted_data(user.id, user.name, user.password)
+                return UserEntity(user.id, user.name, user.password)
             except Exception as e:
                 db.session.rollback()
                 raise e
